2bbf1d3e4468a8e768571a7aa54e09ada72146c9
Nicholas Hooley <nicholashooley27@yahoo.co.uk>
2023-10-18 14:10:10 +0100

    update github links. add profile pic

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -5,7 +5,7 @@ export default function MyProfilePic() {
     <section className="w-full mx-auto">
       <Image
         className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-        src="/images/profile-photo-600x600.png"
+        src="/images/nick-profile.jpeg"
         width={200}
         height={200}
         alt="Nick Hooley"
